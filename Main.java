1d402f7f6518c00ce3e0082fced7d5272841c5d2
Tuğba Örnek <deryanecmettin61@gmail.com>
2022-07-27 16:46:33 +0300

    calculator
    class'lar ile hesap makinesi yapımı

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+Calculator dortislem = new Calculator();
+System.out.println(dortislem.bol(10,2));
+        System.out.println(dortislem.carp(5,1));
+        System.out.println( dortislem.cikar(7,2));
+        System.out.println (dortislem.topla(3,2));
+    }
+}
